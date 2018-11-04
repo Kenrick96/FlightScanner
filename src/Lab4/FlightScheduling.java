dc24bd6852b8e21899fa9a0a93cf0255d8fc822f
Jason <c170127@e.ntu.edu.sg>
2018-11-04 20:28:28 +0800

    minor change

--- src/Lab4/FlightScheduling.java
+++ src/Lab4/FlightScheduling.java
@@ -77,6 +77,7 @@ public class FlightScheduling
 				System.out.println("\nSearch time: " + newResult.getSearchTime() + "ns\n");
 				
 				results.add(newResult);
+				graph.clear();
 			}
 		}
 	}
--- src/tests/TestGraphGenerator.java
+++ src/tests/TestGraphGenerator.java
@@ -9,7 +9,7 @@ public class TestGraphGenerator {
 	public static void main(String[] args) throws FileNotFoundException
 	{
 		GraphGenerator graphGen= new GraphGenerator(50,0.5);
-		graphGen.readCities("Cities.csv");
+//		graphGen.readCities("Cities.csv");
 		graphGen.buildEdges();
 		Graph g= graphGen.getGraph();
 		System.out.println(g.toString());
